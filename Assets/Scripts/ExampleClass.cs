17e9583eeea0f993523c64183d5cbb1ef9926fd1
FabioWienerroither <fabio.wienerroither04@htl-salzburg.ac.at>
2020-11-09 14:10:10 +0100

    FirstHour

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    // Deklaration einer Klasse
+
+    // public           -> access modifier (Zugriffsrechte) | keyword (vorreservierte Schlüsselwörter)
+    // class            -> keyword
+    // ExampleClass     -> identifier (Name der Klasse)
+    // : MonoBehaviour  -> Erstellen eine Unterklasse von MonoBehaviour
+
+
+    // Klasse: Saeugetiere  -> Unterklasse: Hund
+                            -> Unterklasse: Katze
+}
